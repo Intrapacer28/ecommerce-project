0fe4f221f609bb6e87fdf5255a930752d4980710
Pranav Ahire <1234pranavgo@gmail.com>
2024-11-04 10:48:33 +0530

    a change

--- src/backend/payment.js
+++ src/backend/payment.js
@@ -1,22 +1,23 @@
-// api/payment.js
-const express = require('express');
 const cors = require('cors');
 
-const router = express.Router();
-router.use(cors());
+const handler = (req, res) => {
+  cors()(req, res, () => {
+    if (req.method === 'POST') {
+      console.log('Request Body:', req.body);
+      const { shippingInfo, cartItems, totalPrice } = req.body;
 
-// POST payment route
-router.post('/', (req, res) => {
-  console.log('Request Body:', req.body);
-  const { shippingInfo, cartItems, totalPrice } = req.body;
+      const paymentSuccessful = Math.random() > 0.5; // Simulate payment processing
 
-  const paymentSuccessful = Math.random() > 0.5; // Simulate payment processing
+      if (paymentSuccessful) {
+        return res.status(200).json({ success: true, message: 'Payment processed successfully!' });
+      } else {
+        return res.status(400).json({ success: false, message: 'Payment failed. Please try again.' });
+      }
+    } else {
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+  });
+};
 
-  if (paymentSuccessful) {
-    return res.status(200).json({ success: true, message: 'Payment processed successfully!' });
-  } else {
-    return res.status(400).json({ success: false, message: 'Payment failed. Please try again.' });
-  }
-});
-
-module.exports = router;
+export default handler;
