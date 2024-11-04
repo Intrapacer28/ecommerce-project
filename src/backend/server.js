2e8f488f348febc3d63868eda7021e354d3f0d4a
Pranav Ahire <1234pranavgo@gmail.com>
2024-11-04 16:03:55 +0530

    'p'

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const paymentRoutes = require('./api/payment'); // Adjust path as necessary
+const paymentRoutes = require('../api/payment'); // Adjust path as necessary
 
 const app = express();
 const PORT = process.env.PORT || 5000;
